3352487d6514ee84897e4d53bf12109d81983375
Josh Jones <ohblonddev@gmail.com>
2020-04-05 11:25:11 +0100

    Initial Commit

---
+++ lesson/app.py
@@ -0,0 +1,11 @@
+
+def main():
+    print(hello_world())
+
+
+def hello_world() -> str:
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/hello_world_test.py
@@ -0,0 +1,7 @@
+import unittest
+from lesson.app import hello_world
+
+
+class TestApp(unittest.TestCase):
+    def test_hello_world_says_hello_world(self):
+        self.assertEqual("Hello World!", hello_world())
